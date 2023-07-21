3bfcd4728fff96312eb1f6a5d95e933a6e53429a
Corentin Mors <corentin@dashlane.com>
2023-07-21 09:47:00 +0200

    Remove specific asset path for github pages

--- website/next.config.js
+++ website/next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
 let assetPrefix = '';
 let basePath = '';
-
-if (isGithubActions) {
-    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-
-    assetPrefix = `/${repo}/`;
-    basePath = `/${repo}`;
-}
 
 module.exports = {
     output: 'export',
