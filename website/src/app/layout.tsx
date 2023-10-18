4b58ca856f2218c45cede4a116c004c218218edc
Corentin Mors <corentin@dashlane.com>
2023-10-18 13:36:00 +0200

    Improve passkeys directory SEO (#23)

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -5,7 +5,45 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
     title: "Passkeys Directory",
-    description: "Domains compatible with Passkeys",
+    description: "Explore the crowd-sourced index of websites, apps, and services that offer signing in with passkeys.",
+    icons: [
+        {
+            src: "/favicon.ico",
+            sizes: "any",
+            type: "image/x-icon",
+        },
+    ],
+    backgroundColor: "#c6dadf",
+    themeColor: "#c6dadf",
+    display: "standalone",
+    orientation: "portrait",
+    scope: "/",
+    startUrl: "/",
+    lang: "en",
+    dir: "ltr",
+    metadataBase: "https://passkeys-directory.dashlane.com/",
+    openGraph: {
+        type: "website",
+        locale: "en_US",
+        url: "https://passkeys-directory.dashlane.com/",
+        title: "Passkeys Directory",
+        description:
+            "Explore the crowd-sourced index of websites, apps, and services that offer signing in with passkeys.",
+        site_name: "Passkeys Directory",
+        images: [
+            {
+                url: "https://passkeys-directory.dashlane.com/og-image.webp",
+                width: 1600,
+                height: 900,
+                alt: "Passkeys Directory",
+            },
+        ],
+    },
+    twitter: {
+        card_type: "summary_large_image",
+        site: "@dashlane",
+        creator: "@dashlane",
+    },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
